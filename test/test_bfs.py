909ac543c5f0ceb0c09d0548835e6713bb435737
Grace Ramey <graceramey@pcd-ak2q05d-lt.ucsf.edu>
2022-01-19 00:30:08 -0800

    updated mini network tests

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -11,6 +11,25 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
+    
+    filename="./data/tiny_network.adjlist" #get relative file path
+    G=Graph(filename) #create instance of graph class called G
+    #assert that all nodes are being traversed
+    for node in G.graph.nodes():
+        #check that a few random nodes output the right order of traversal
+        if node=="31806696":
+            assert G.bfs(node)==['31806696', 'Martin Kampmann', 'Lani Wu', 'Luke Gilbert', '31626775', '32790644', '31540829', '33483487', '34272374', '32353859', '33232663', '32042149', '31395880', '30727954', '32036252', 'Neil Risch', 'Steven Altschuler', 'Michael McManus', 'Nevan Krogan', 'Marina Sirota', 'Atul Butte', 'Michael Keiser', 'Charles Chiu', 'Hani Goodarzi', '29700475', '32025019', '30944313', '33765435', '31486345', '33242416']
+        
+        if node=="Marina Sirota":
+            assert G.bfs(node)==['Marina Sirota', '33765435', '31486345', '30944313', '32353859', 'Atul Butte', 'Michael Keiser', 'Hani Goodarzi', 'Nevan Krogan', 'Martin Kampmann', '31395880', '32025019', '33242416', '33232663', '29700475', '32042149', '31540829', '34272374', '31626775', '32790644', '31806696', '33483487', 'Lani Wu', 'Steven Altschuler', 'Michael McManus', 'Charles Chiu', 'Neil Risch', 'Luke Gilbert', '30727954', '32036252']
+            
+        if node=="32042149":
+            assert G.bfs(node)==['32042149', 'Lani Wu', 'Hani Goodarzi', '32790644', '31806696', '31395880', '30727954', '32036252', '33232663', '32025019', '30944313', 'Martin Kampmann', 'Steven Altschuler', 'Luke Gilbert', 'Atul Butte', 'Michael McManus', 'Michael Keiser', 'Charles Chiu', 'Marina Sirota', 'Nevan Krogan', '31626775', '31540829', '33483487', '34272374', '32353859', '33765435', '33242416', '29700475', '31486345', 'Neil Risch']
+            
+            
+        #assert that all nodes are being traversed if no end node is supplied and no matter the start node
+        assert len(G.bfs(node)) == 30
+    
     pass
 
 def test_bfs():
